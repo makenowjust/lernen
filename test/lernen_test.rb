510da5bc343f053c248c1a3bb4589bb0af456791
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-08-29 04:47:10 +0900

    Update test params

--- test/lernen_test.rb
+++ test/lernen_test.rb
@@ -3,7 +3,12 @@
 require_relative "test_helper"
 
 class TestLernen < Minitest::Test
-  table = [
+  N = 30
+
+  ORACLE = :random_walk
+  ORACLE_PARAMS = { step_limit: 1500 }.freeze
+
+  TEST_CASES = [
     [:lstar, { cex_processing: :binary }],
     [:lstar, { cex_processing: :linear }],
     [:lstar, { cex_processing: :exponential }],
@@ -12,12 +17,12 @@ class TestLernen < Minitest::Test
     [:kearns_vazirani, { cex_processing: :linear }],
     [:kearns_vazirani, { cex_processing: :exponential }],
     [:lsharp, {}]
-  ]
+  ].freeze
 
-  table.each do |(algorithm, params)|
+  TEST_CASES.each do |(algorithm, params)|
     define_method(:"test_learn_dfa_#{algorithm}_#{params}") do
       alphabet = %w[0 1 2]
-      50.times do |seed|
+      N.times do |seed|
         random = Random.new(seed * 41)
         expected = Lernen::DFA.random(alphabet:, random:, min_state_size: 8, max_state_size: 10)
 
@@ -25,24 +30,22 @@ class TestLernen < Minitest::Test
           Lernen.learn(
             alphabet:,
             sul: expected,
-            oracle: :random_walk,
-            oracle_params: {
-              step_limit: 1500
-            },
+            oracle: ORACLE,
+            oracle_params: ORACLE_PARAMS,
             algorithm:,
             params:,
             random:
           )
         cex = expected.check_equivalence(alphabet, result)
 
-        assert_nil cex, "Equivalence check failed (seed: #{seed}, cex: #{cex})"
+        assert_nil cex, "Equivalence check failed (seed: #{seed})"
       end
     end
 
     define_method(:"test_learn_moore_#{algorithm}_#{params}") do
       alphabet = %w[0 1 2]
       output_alphabet = %w[a b c d]
-      50.times do |seed|
+      N.times do |seed|
         random = Random.new(seed * 41)
         expected = Lernen::Moore.random(alphabet:, output_alphabet:, random:, min_state_size: 8, max_state_size: 10)
 
@@ -51,24 +54,22 @@ class TestLernen < Minitest::Test
             alphabet:,
             sul: expected,
             automaton_type: :moore,
-            oracle: :random_walk,
-            oracle_params: {
-              step_limit: 1500
-            },
+            oracle: ORACLE,
+            oracle_params: ORACLE_PARAMS,
             algorithm:,
             params:,
             random:
           )
         cex = expected.check_equivalence(alphabet, result)
 
-        assert_nil cex, "Equivalence check failed (seed: #{seed}, cex: #{cex})"
+        assert_nil cex, "Equivalence check failed (seed: #{seed})"
       end
     end
 
     define_method(:"test_learn_mealy_#{algorithm}_#{params}") do
       alphabet = %w[0 1 2]
       output_alphabet = %w[a b c d]
-      50.times do |seed|
+      N.times do |seed|
         random = Random.new(seed * 41)
         expected = Lernen::Mealy.random(alphabet:, output_alphabet:, random:, min_state_size: 8, max_state_size: 10)
 
@@ -77,17 +78,15 @@ class TestLernen < Minitest::Test
             alphabet:,
             sul: expected,
             automaton_type: :mealy,
-            oracle: :random_walk,
-            oracle_params: {
-              step_limit: 1500
-            },
+            oracle: ORACLE,
+            oracle_params: ORACLE_PARAMS,
             algorithm:,
             params:,
             random:
           )
         cex = expected.check_equivalence(alphabet, result)
 
-        assert_nil cex, "Equivalence check failed (seed: #{seed}, cex: #{cex})"
+        assert_nil cex, "Equivalence check failed (seed: #{seed})"
       end
     end
   end
